8fbd52cbd81a35096302d4024c028fb61e527de1
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-11-11 16:18:41 +0100

    cleanup unused code and extra newlines

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const displayText = document.getElementById('display-text');
 const weatherArray = [];
 
 // LYSSNARE Searchfield = key down & click
-let theWeather 
+let theWeather
 
 
 searchField.addEventListener('keydown', async (event) => {
   if (event.key === 'Enter') {
     let searchValue = searchField.value;
     searchField.value = '';
-    
+
     let weatherData = await getWeatherFromCity(searchValue)
     theWeather = new Weather(weatherData)
 
@@ -26,8 +26,8 @@ searchField.addEventListener('keydown', async (event) => {
     theWeather.createWeatherCard()
     theWeather.changeUnit(TemperatureConverter.cToF, theWeather.temperature, "f")
 
+    //hidden text for screen readers
     displayText.innerHTML = returnDisplayText(weatherData);
-//    addBoxData(weatherData);
   }
 
 });
@@ -61,84 +61,9 @@ function returnDisplayText(data) {
   return `<p>Idag klockan ${data.time} i ${data.city} är det ${data.weather} med en temperatur på <span id="display-temp">${temp}</span> <span id="display-unit">${unit}</span> grader.</p>`
 }
 
-
-// Function to modify Boxes
-function addBoxData(data) {
-  // FIRST box
-  const tempEl = document.getElementById('temp');
-  const unitType = document.getElementById('unit');
-  const unitDescription = document.getElementById('toggle-scale');
-
-  let unit = '<i class="ri-celsius-line"></i>';
-  let shownTemp = data.temperature; // bas: Celsius
-  unitDescription.textContent = 'Celsius';
-
-  if (checkbox.checked) {
-    unit = '<i class="ri-fahrenheit-line"></i>';
-    unitDescription.textContent = 'Fahrenheit';
-    shownTemp = TemperatureConverter.cToF(data.temperature); // C → F
-  }
-
-  tempEl.textContent = shownTemp;
-  unitType.innerHTML = unit;
-
-  // MIDDLE box
-  const weather = document.getElementById('weather');
-  weather.textContent = data.weather;
-
-  // LAST box
-  const time = document.getElementById('time');
-  time.textContent = data.time;
-
-  const location = document.getElementById('location');
-  location.textContent = data.city;
-}
-
-
-
-
 // Toggle temp
 const checkbox = document.getElementById("unitSwitch"); // Hämtar checkbox-elementet från HTML med id "unitSwitch"
 
-/*
-
-
-
-checkbox.addEventListener('change', () => {
-  const displayTemp = document.getElementById('display-temp');
-  const displayUnit = document.getElementById('display-unit');
-
-  displayTemp.textContent = convertTemperature(tempC);
-  displayUnit.innerHTML = '<i class="ri-fahrenheit-line"></i>';
-} )
-
-
-*/
-
-
-
-
-
-
-
-
-
-// TEMPERATURE UNIT CONVERT (c/f)
-
-
-// function convertTemperature(temperature) {
-//   // Skapar en funktion som konverterar temperatur mellan Celsius och Fahrenheit
-
-//   let fahrenheit = checkbox.checked; // Kontrollerar om checkboxen är markerad; true om markerad, false annars
-//   if (!fahrenheit) {
-//     // Om checkboxen är markerad
-
-//     return Math.round(((temperature - 32) * 5 / 9) * 10) / 10; // Konvertera från Fahrenheit till Celsius och runda till en decimal // Formeln: (F - 32) * 5/9
-//   } else {
-//     // Om checkboxen inte är markerad (inputen är i Celsius)
-//     return Math.round((temperature * (9 / 5) + 32) * 10) / 10; // Konvertera från Celsius till Fahrenheit och runda till en decimal // Formeln: C * 9/5 + 32
-//   }
-// }
 
 // Växla C ↔ F när knappen byts
 checkbox.addEventListener('change', () => {
@@ -169,10 +94,6 @@ checkbox.addEventListener('change', () => {
 
   console.log(`Växlat till ${showF ? 'Fahrenheit' : 'Celsius'}: ${newVal}`);
 });
-
-
-
-
 
 // ------------- ADD 2 WATCHLIST
 
