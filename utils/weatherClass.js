699cdd82156f780d67499feefb8e5b9032d98d7a
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-11-11 11:31:13 +0100

    continuing to iterate on the weather class

--- utils/weatherClass.js
+++ utils/weatherClass.js
@@ -1,5 +1,5 @@
 import { getWeatherFromCity } from "../services/meteo.js";
-import { getWeatherIcon } from "./getWeatherIcon.js";
+import { TemperatureConverter } from "../temperature.js";
 
 export class Weather {
 
@@ -7,31 +7,52 @@ export class Weather {
         this.city = data.city,
         this.temperature = data.temperature,
         this.weather = data.weather,
-        this.icon = getWeatherIcon(data.weatherCode),
+        this.icon = data.icon,
         this.time = data.time,
         this.date = data.date,
-        this.timeZone = data.timezone
+        this.timeZone = data.timezone,
+        this.unitC = '<i class="ri-celsius-line"></i>',
+        this.unitF = '<i class="ri-fahrenheit-line"></i>'
     }
 
     createWeatherCard() {
         const weatherContainer = document.getElementById("weather-container")
-        let card = document.createElement("div")
-        card.classList.add("weather-card")
-        card.innerHTML = `<h2>${this.city} ${this.icon}</h2><p><span>${this.temperature}</span><span>${this.weather}</span></p>`
+        this.card = document.createElement("div")
+        this.card.classList.add("weather-card")
+        this.title = document.createElement("h2");
+        this.title.innerHTML = `${this.city} ${this.icon}`
+        this.paragraph = document.createElement("p")
+        this.paragraph.innerHTML = `<span class="temp">${this.temperature}</span><span class="unit">${this.unitC}</span> <span class="description">${this.weather}</span>`
+        this.card.appendChild(this.title);
+        this.card.appendChild(this.paragraph);
         weatherContainer.replaceChildren("")
-        weatherContainer.appendChild(card)
+        weatherContainer.appendChild(this.card)
     }
 
     async updateWeather() {
         const weatherData = await getWeatherFromCity(this.city);
-        this.temperature = weatherData.temperature
-        this.weather = weatherData.weather
-        this.time = weatherData.time
-        this.date = weatherData.date
+        this.temperature = weatherData.temperature;
+        this.weather = weatherData.weather;
+        this.icon = weatherData.icon;
+        this.time = weatherData.time;
+        this.date = weatherData.date;
     }
 
     updateWeatherCard() {
-        
+        this.updateWeather()
+        this.createWeatherCard()
+    }
+
+    changeUnit(func, temperature, changeToUnit) {
+        let temp = this.card.querySelector(".temp");
+        temp.textContent = func(temperature);
+        let unit = this.card.querySelector(".unit");
+        console.log(changeToUnit)
+        if (changeToUnit === "f") {
+            unit.innerHTML = this.unitF
+        } else {
+            unit.innerHTML = this.unitC
+        }
     }
 
     addToWatchlist() {
