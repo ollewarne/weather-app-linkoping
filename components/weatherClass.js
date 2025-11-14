e0c7be084a5c1ad833be905cb809dceb39cd32c3
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2025-11-14 10:55:57 +0100

    refactor changeUnit to changeTemperatureAndUnit

--- components/weatherClass.js
+++ components/weatherClass.js
@@ -1,4 +1,5 @@
 import { getWeatherFromCity } from "../services/meteo.js";
+import { temperatureConverter, unitConverter } from "../utils/temperatureConverter.js";
 
 export class Weather {
 
@@ -10,12 +11,10 @@ export class Weather {
         this.time = data.time,
         this.date = data.date,
         this.timeZone = data.timezone,
-        this.unitC = '<i class="ri-celsius-line"></i>',
-        this.unitF = '<i class="ri-fahrenheit-line"></i>'
+        this.unit = '℃' //DEFAULT 
 
         this.weatherContainer = document.getElementById("weather-container")
         this.watchlist = document.getElementById("watchlist")
-
     }
 
     createWeatherCard() {
@@ -24,7 +23,7 @@ export class Weather {
         this.title = document.createElement("h2");
         this.title.innerHTML = `${this.city} ${this.icon}`
         this.paragraph = document.createElement("p")
-        this.paragraph.innerHTML = `<span class="temp">${this.temperature}</span><span class="unit">${this.unitC}</span> <span class="description">${this.weather}</span>`
+        this.paragraph.innerHTML = `<span class="temp">${this.temperature}</span><span class="unit">${this.unit}</span> <span class="description">${this.weather}</span>`
         this.card.appendChild(this.title);
         this.card.appendChild(this.paragraph);
         this.weatherContainer.replaceChildren("")
@@ -45,17 +44,17 @@ export class Weather {
         this.createWeatherCard()
     }
 
-    changeUnit(func, temperature, changeToUnit) {
+    changeTemperatureAndUnit() {
+
+        this.temperature = temperatureConverter(this.temperature);
+        this.unit = unitConverter(this.unit);
+
         let temp = this.card.querySelector(".temp");
-        temp.textContent = func(temperature);
+        temp.textContent = this.temperature;
+
         let unit = this.card.querySelector(".unit");
-        console.log(changeToUnit)
-        if (changeToUnit === "f") {
-            unit.innerHTML = this.unitF
-        } else {
-            unit.innerHTML = this.unitC
-        }
-    }
+        unit.textContent = this.unit;
+    };
 
     addToWatchlist() {
         this.weatherContainer.removeChild(this.card)
